f22c6a2822ec4c79dde376b3180e380cb0ac3a59
John Wilson <john@digilog-n.canvasslabs.com>
2020-09-22 14:10:27 -0700

    Commit a couple run scripts

---
+++ ArrowPlasmaTestJava/run_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$JAVA_HOME/bin/java -jar ~/DigiLog_repositories/Arrow/ArrowPlasmaTestJava/build/libs/ArrowPlasmaTest.jar
---
+++ PHM08_to_Plasma/run_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$JAVA_HOME/bin/java -jar ~/DigiLog_repositories/Arrow/PHM08_to_Plasma/build/libs/PHM08_to_Plasma.jar ~/DigiLog_repositories/Data/PHM08/train.txt
